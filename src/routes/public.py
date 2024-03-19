8c82aa26868be3870f2eed6a9c048db27ef345a3
John <johnzunigacastillo@gmail.com>
2024-03-19 15:50:01 +0800

    add register route

--- src/routes/public.py
+++ src/routes/public.py
@@ -1,8 +1,28 @@
-from flask import Blueprint
+from flask import Blueprint,request,render_template
+from src.model.user import User
+from src.model.database import db
 
 public = Blueprint('public',__name__)
-
 
 @public.route('/')
 def home():
     return "You're home!"
+
+@public.route('/register', methods=['GET','POST'])
+def register():
+    
+    if request.method == "POST":
+        username = request.form.get('username')
+        password = request.form.get('password')
+        
+        user = User(username,password)
+        
+        db.session.add(user)
+        db.session.commit()
+        
+        return "ok"
+    
+    else:
+        return render_template('/pages/register.html')
+
+    
