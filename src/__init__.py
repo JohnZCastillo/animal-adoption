f037796e7650c48b048e37b5bf85492e9fda955d
John <johnzunigacastillo@gmail.com>
2024-03-19 23:28:15 +0800

    add authentication

--- src/__init__.py
+++ src/__init__.py
@@ -1,11 +1,11 @@
-from flask import Flask
-from .routes.public import public
+from flask import Flask,render_template
+from .routes.auth import auth
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from dotenv import load_dotenv
 from .model.user import User
 from .model.database import db
-
+from flask_login import LoginManager,login_required,login_user
 import os
 
 def create_app():
@@ -15,12 +15,28 @@ def create_app():
     
     app = Flask(__name__)
 
-    app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('db')
-
+    app.secret_key = os.getenv('SECRET')
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DB')
+    
     db.init_app(app)
         
     migrate = Migrate(app,db)
     
-    app.register_blueprint(public)
+    login_manager = LoginManager()
+    login_manager.init_app(app)
+    
+    @login_manager.user_loader
+    def load_user(id):
+        try:
+            return db.session.get_one(User,id)
+        except:
+            return None
+        
+    app.register_blueprint(auth)
+    
+    @app.route('/homepage')
+    @login_required
+    def homepage():
+        return render_template('/pages/homepage.html')
     
     return app
