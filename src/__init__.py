092894787912e58f08fb728d06052208c367a76f
John <johnzunigacastillo@gmail.com>
2024-03-20 13:40:58 +0800

    fix import

--- src/__init__.py
+++ src/__init__.py
@@ -8,18 +8,18 @@ from .model.database import db
 from flask_login import LoginManager,login_required,login_user
 import os
 
+env_path = os.path.join(os.path.dirname(__file__),'.env')
+load_dotenv(env_path)
+    
 def create_app():
 
-    env_path = os.path.join(os.path.dirname(__file__),'.env')
-    load_dotenv(env_path)
-    
     app = Flask(__name__)
-
+    
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DB_DEVELOPMENT')    
     app.secret_key = os.getenv('SECRET')
-    app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DB_DEVELOPMENT')
     
     db.init_app(app)
-        
+    
     migrate = Migrate(app,db)
     
     login_manager = LoginManager()
@@ -39,5 +39,6 @@ def create_app():
     @login_required
     def homepage():
         return render_template('/pages/homepage.html')
+    
     
     return app
